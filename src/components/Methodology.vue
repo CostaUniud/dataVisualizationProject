f13f46afa27e3d3d88b8014a295e7aa62ac23a64
CostaUniud <l.costantino@icloud.com>
2020-06-05 22:37:27 +0200

    fine si spera xD

--- src/components/Methodology.vue
+++ src/components/Methodology.vue
@@ -2,6 +2,12 @@
   <div class="methodology--paragraph paragraph">
     <h3 class="heading-tertiary">Methodology</h3>
     <br>
+    <h4>The purpose</h4>
+    <p>
+      The intent of this visualization is, on the one hand, to show general information on suicides in the world (numbers, gender, age) and on the other
+      to understand if factors such as happiness and the GDP of a country can affect the number of suicides.
+    </p>
+    <h4>Datasets</h4>
     <p>
       The data are available at the following links:
       <ul>
@@ -13,29 +19,37 @@
         </li>
       </ul>
       <br>
-      The first dataset consists of the number of suicides each year in a given country, split by sex and age groups, plus the total population in according group,
-      thus allowing us to calculate the crude rates per 100k people. The use of rates seems obvious, as the sheer number of suicides corresponds to the size of the population:
-      the bigger the country, the more suicides.
+      As regards the first dataset, some premises must be made regarding data quality.
+      The dataset was collected by putting together the data of the World Health Organization in 2018. WHO assigns to each country a level of quality,
+      and therefore of reliability of the data. The WHO reports that, of its 183 Member States for which suicide estimates are published, only 80 have good-quality vital
+      registration data that can be used directly to estimate suicide rates (for more information see the WHO’s
+      <a href="https://www.who.int/mental_health/suicide-prevention/mortality_data_quality/en/">Quality of suicide mortality data</a>).
+      <br>
+      The second dataset is instead a collection of different World Happiness Reports published annually by the United Nations Sustainable Development Solutions Network.
+      The rankings of national happiness are based on a Cantril ladder survey. Nationally representative samples of respondents are asked to think of a ladder,
+      with the best possible life for them being a 10, and the worst possible life being a 0. They are then asked to rate their own current lives on that 0 to 10 scale.
+      The report correlates the results with various life factors.
     </p>
     <h4>The map</h4>
     <p>
-      Qui si sono utilizzati i tassi di suicidi ogni 100.000 abitanti per ogni singolo Paese ogni anno.
+      The first dataset consists of the number of suicides each year in a given country, split by sex and age groups, plus the total population in according group,
+      thus allowing us to calculate the crude rates per 100k people. The use of rates seems obvious, as the sheer number of suicides corresponds to the size of the population:
+      the bigger the country, the more suicides.
+      The map shows the rates of each individual country per year.
     </p>
     <h4>Total world suicides per year</h4>
     <p>
-      &Eacute; stata fatta la somma dei suicidi in tutti i Paesi per ogni anno. In oltre &egrave; stata aggiuta la percetuale in base al sesso e l&rsquo;et&agrave;.
+      For each year the total number of suicides is shown, then divided by gender and age group.
     </p>
     <h4>Trend suicides per country</h4>
     <p>
-      Qui sono stati utilizzati i numeri di suicidi per Paese, mostrando il trend del numero di quest&rsquo;ultimi ogni anno per ogni singolo Paese.
+      Suicide numbers by country over the years are shown.
     </p>
     <h4>Relation suicides/happiness in 2015</h4>
     <p>
-      Sono stati presi i dati sui suicidi e sugli indici di felicit&agrave; dei vari Paesi nel 2015 per mostrare la relazione tra tasso di suicidio e felicità nei vari Paesi.
-      Quest'ultimo viene espresso in una classifica dei Paesi, dove al primo posto c'è il Paese più felice al mondo. Quindi sulle ascisse del grafico la felicità diminuirà
-      andando verso destra.
+      The relationship between suicides and happiness of the various countries in 2015 is shown.
     </p>
     <h4>Relation suicides/gdp per year</h4>
-    <p></p>
+    <p>The relationship between suicides and GDP of the various countries over the years is shown.</p>
   </div>
 </template>
