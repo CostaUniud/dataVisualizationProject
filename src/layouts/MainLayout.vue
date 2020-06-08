609c226585ad6fe101b4141d5b66a01a7c8206f1
CostaUniud <l.costantino@icloud.com>
2020-06-08 15:06:53 +0200

    flexbox+logo

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,10 +2,15 @@
   <q-layout view="lHh Lpr lFf">
     <q-header>
       <q-toolbar>
+        <img src="statics/logo.png" alt="Uniud logo" class="logo">
         <q-toolbar-title>
           <h1>Suicides in the world</h1>
           <h4 class="heading-quaternary">Visual languages for complex systems's project - University of Udine</h4>
         </q-toolbar-title>
+        <div class="q-toolbar-author">
+          <span class="q-toolbar-author__name">Pepe Federico, professor</span>
+          <span class="q-toolbar-author__name">Costantino Luca, student</span>
+        </div>
       </q-toolbar>
     </q-header>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,56 +3,50 @@
     <Map :fieldYear="fieldYear"/>
     <aside ref="aside">
       <q-scroll-observer @scroll="scrollHandler"/>
-      <div class="range-field" v-if="position < 1500 || (position >= 3750 && position < 4500)">
+      <div class="range-field" v-if="position < (pageHeight * 2 - 250) || (position >= (pageHeight * 4 - 350) && position < (pageHeight * 4 + 200))"> <!-- v-if="position < 1900 || (position >= 3950 && position < 4500)" -->
         <p class="range-field__label">1985</p>
         <q-slider v-model="fieldYear" color="negative" :min="1985" :max="2016" vertical label />
         <p class="range-field__label">2016</p>
       </div>
-      <div class="country-field" v-if="position >= 1500 && position < 2800">
+      <div class="country-field" v-if="position >= (pageHeight * 2 - 250) && position < (pageHeight * 3 - 250)">
         <q-select v-model="fieldCountry" :options="country" label="Country"/>
       </div>
-      <div class="country-field" v-if="position >= 2800 && position < 3750">
+      <div class="country-field" v-if="position >= (pageHeight * 3 - 250) && position < (pageHeight * 4 - 350)">
         <q-select v-model="fieldOrder" :options="order" label="Order By"/>
       </div>
     </aside>
-    <section class="flex pie-area" v-if="sex">
-      <h2 class="heading-secondary" style="left: 50%; transform: translate(-50%, 0);">Total world suicides per year</h2>
-      <div class="row" style="margin-top: 7rem">
-        <div class="col-1 offset-md-3" style="width: 10rem;">
+    <section class="total-area" v-if="sex">
+      <h2 class="heading-secondary">Total world suicides per year</h2>
+      <div class="total-area__main">
+        <div class="total-area__number">
           <p><span class="sub-title">Total </span><span class="value">{{ formatMoney(tot) }}</span></p>
           <p class="sex"><span class="sub-title">Male </span><span class="value male">{{ formatMoney(sex.male) }}</span></p>
           <p class="sex"><span class="sub-title">Female </span><span class="value female">{{ formatMoney(sex.female) }}</span></p>
         </div>
-        <div class="col-1 offset-md-6">
+        <div class="total-area__plot">
           <SexAge :fieldYear="fieldYear"/>
         </div>
       </div>
     </section>
-    <section class="line-area">
+    <section class="trend-area">
       <h2 class="heading-secondary">Trend suicides per country</h2>
-      <div class="row">
-        <div class="col-1 offset-md-3">
-          <Trend :fieldCountry="fieldCountry"/>
-        </div>
+      <div class="trend-area__plot">
+        <Trend :fieldCountry="fieldCountry"/>
       </div>
     </section>
     <section class="lollipop-area">
       <h2 class="heading-secondary">Relation suicides/happiness in 2015</h2>
-      <div class="row lollipop">
-        <div class="col-1 offset-md-2">
-          <Happy :fieldOrder="fieldOrder"/>
-        </div>
+      <div class="lollipop-area__plot">
+        <Happy :fieldOrder="fieldOrder"/>
       </div>
     </section>
-    <section class="lollipop-area">
+    <section class="lollipop-area" style="margin-bottom: 10rem">
       <h2 class="heading-secondary">Relation suicides/gdp per year</h2>
-      <div class="row lollipop">
-        <div class="col-1">
-          <PIL :fieldYear="fieldYear"/>
-        </div>
+      <div class="lollipop-area__plot" style="margin-left: -20rem">
+        <PIL :fieldYear="fieldYear"/>
       </div>
     </section>
-    <q-separator inset color="gray" style="height: 1px; margin: 0 20rem"/>
+    <q-separator inset color="gray" />
     <section class="methodology">
       <Methodology />
     </section>
@@ -86,7 +80,8 @@ export default {
       ],
       position: 0,
       fieldCountry: 'Albania',
-      fieldOrder: 'Rank Happiness'
+      fieldOrder: 'Rank Happiness',
+      pageHeight: 0
     }
   },
   components: {
@@ -118,6 +113,7 @@ export default {
     }),
     formatMoney,
     scrollHandler () {
+      this.pageHeight = window.screen.height
       this.position = getScrollPosition(getScrollTarget(this.$refs.aside))
     }
   }
